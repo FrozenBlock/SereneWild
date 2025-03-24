f02fb0f89dff8054efc69859f6179743c3165510
AViewFromTheTop <AViewFromTheTop@users.noreply.github.com>
2025-03-24 22:51:34 +0000

    Apply automatic changes

--- src/main/java/net/frozenblock/serenewild/datagen/tag/SereneWildBlockTagProvider.java
+++ src/main/java/net/frozenblock/serenewild/datagen/tag/SereneWildBlockTagProvider.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2023-2025 FrozenBlock
- * This file is part of Wilder Wild.
+ * Copyright 2025 FrozenBlock
+ * This file is part of Springier Life.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
